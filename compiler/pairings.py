56d2de70eb3d58c96c6d370ed45204e2413a97c4
Fasjul <fasjul@gmail.com>
2019-04-16 19:31:21 +0200

    writing wrapper

--- compiler/pairings.py
+++ compiler/pairings.py
@@ -1,56 +1,45 @@
 from bplib.bp import BpGroup, G1Elem, G2Elem, GTElem
 from SigmaProtocol import *
-#class MyBpGroup(BpGroup):
+
+
+class MyBpGroup:
+    def __init__(self, bp):
+        self.bp = bp
+
+    def groups(self):
+        return self.bp.gen1().group, self.bp.gen2().group
+
+    def infinite(self):
+        g1, g2 = self.bp.gen1(), self.bp.gen2()
+        gT = self.bp.pair(g1, g2)
+        return gT.zero(self.bp)
+
+    def order(self):
+        return self.bp.order()
+
+    def generator(self):
+        return self.bp.pair(self.bp.gen1(), self.bp.gen2())
 
 
 
-""" def enhance_method(klass, method_name, replacement):
-    'replace a method with an enhanced version'
-    method = getattr(klass, method_name)
-    def enhanced(*args, **kwds): return replacement(method, *args, **kwds)
-    setattr(klass, method_name, new.instancemethod(enhanced, None, klass))
+class AdditivePoint:
+    def __init__(self, pt):
+        self.pt = pt
 
-def method_logger(old_method, self, *args, **kwds):
-    'example of enhancement: log all calls to a method'
-    print '*** calling: %s%s, kwds=%s' % (old_method._ _name_ _, args, kwds)
-    return_value = old_method(self, *args, **kwds) # call the original method
-    print '*** %s returns: %r' % (old_method._ _name_ _, return_value)
-    return return_value
-"""
-def additive_scalar(self, scalar):
-    return self**scalar
+    def export(self, form=POINT_CONVERSION_COMPRESSED):
+        return self.pt.export(form)
 
-#enhance self.mul with scalars so it returns self.exp (DLRep tries to multiply xigi)
-#enhance (create?) self.add with group elements so it calls self.mul (DLRep tries to add all the xigi)
+    def __eq__(self, other):
+        return self.pt == other.pt
 
-G = BpGroup()
-Zp = G.order()
+    def __add__(self, other):
+        return self.pt*(other.pt)
 
-Ga = Zp.random() * G.gen1()
-Ha = Zp.random() * G.gen2()
-k = G.pair(Ga, Ha)
-print(k.group.order())
-y = k+k # same object! ???
-u = k**Zp.random()
-print("\nk:", k, "\nk+k:", k+k, "\nk*k", k*k, "\nk**2", k**2)
-print("Set u = k**Zp.random()")
-print("\nu:",u,"\nu+u:", u+u, "\nu*u:",u*u, "\nu**2:",u**2)
+    def __mul__(self, nb):
+        return self.pt**nb
 
 
-print(u+u == u*u)
-print(u*u == u**2)
 
-
-def groups(GTgroup):
-    return GTgroup.gen1().group, GTgroup.gen2().group
-
-def infinite(GTgroup):
-    G1, G2 = groups(GTgroup)
-    return GTgroup.pair(G1.infinite(), G2.infinite())
-
-def generator(GTgroup):
-    G1, G2 = groups(GTgroup)
-    return (GTgroup.pair(G1.generator(), G1.generator()))
 
 
 
