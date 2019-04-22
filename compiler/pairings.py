dee018010f337e0bcaf4a699e1d7c209705863f0
Fasjul <fasjul@gmail.com>
2019-04-22 19:49:10 +0200

    finished wrapper. PairablePoint and point.group to discuss. testdoc done, not so pretty

--- compiler/pairings.py
+++ compiler/pairings.py
@@ -1,58 +1,63 @@
 from bplib.bp import BpGroup, G1Elem, G2Elem, GTElem
 from SigmaProtocol import *
 
-"""
-check everything because bidule.group actually returns the GT group and not the G1/G2 groups (do our classes mimic them well?)
-"""
+def test():
+    """
+    Some wrappers to use on top of Bplib groups so groups and points behave well with most of the methods used in petlib.ec
 
-class AdditivePoint:
-    def __init__(self, pt):
-        self.pt = pt
+    Given a group GT defining a pairing e(G1,G2)->GT, we create a variant MyGTGRoup which behaves similarly, with more tools:
+    >>> G = BpGroup()
+    >>> mG = MyGTGroup(G)
+    >>> G.order() == mG.order()
+    True
+    >>> g, gmg =G.pair(G.gen1(), G.gen2()), mG.generator()
+    >>> g == gmg.pt
+    True
+    >>> g == mG.pair(mG.gen1(), mG.gen2()).pt
+    True
 
-    def export(self, form=0):
-        return self.pt.export(form) if form else self.pt.export()
+    We define the AdditivePoint class which defines GT points additively:
+    >>> gmg == AdditivePoint(g)
+    True
+    >>> AdditivePoint(g**(g.group.order())) == mG.infinite()
+    True
 
-    def __mul__(self, nb):
-        return self.pt**nb
+    I.e it overrides the multiplicative syntax of Bplib GT points by an additive one
+    >>> r = G.order().random()
+    >>> g1, g1mg = g**r, r*gmg
+    >>> g1 == g1mg.pt
+    True
+    >>> g1*g1*g1 == (g1mg+g1mg+g1mg).pt
+    True
+    >>> g1.export() == g1mg.export()
+    True
+    >>> g1.group == g1mg.group
+    True
 
-    def __eq__(self, other):
-        return self.pt == other.pt
 
-    def __add__(self, other):
-        return self.pt*(other.pt)
-        
+    We also derive G1 and G2 groups from GT
+    >>> G1, G2 = mG.groups()
+    >>> g1, g2 = G1.generator(), G2.generator()
+    >>> G1.infinite() == G.gen1().inf(G)
+    True
+    >>> g1*0 ==G1.infinite()
+    True
+    >>> g1*0 == g1*G.order()
+    True
+    >>> G2.infinite() == G.gen2().inf(G)
+    True
+    >>> g2*0 == G2.infinite()
+    True
+    >>> g2*0 == g2*G.order()
+    True
+    """
+    return 1
 
-    __rmul__=__mul__
-
-
-
-class MyG1Group:
-    def __init__(self, bp):
-        self.bp = bp
-
-    def generator(self):
-        return self.bp.gen1()
-
-    def infinite(self):
-        return self.generator().inf(self.bp)
-
-    def order(self):
-        return self.bp.order()
-
-class MyG2Group:
-    def __init__(self, bp):
-        self.bp = bp
-
-    def generator(self):
-        return self.bp.gen2()
-
-    def infinite(self):
-        return self.generator().inf(self.bp)
-
-    def order(self):
-        return self.bp.order()
 
 class MyGTGroup:
+    """
+    A wrapper for the GT group such that it creates additive points and allows to retrieve groups G1 and G2.
+    """
     def __init__(self, bp):
         self.bp = bp
 
@@ -70,41 +75,101 @@ class MyGTGroup:
     def generator(self):
         return AdditivePoint(self.bp.pair(self.bp.gen1(), self.bp.gen2()))
 
+    def pair(self, p1, p2):
+        return AdditivePoint(self.bp.pair(p1, p2))
+
+    def gen1(self):
+        return self.bp.gen1()
+
+    def gen2(self):
+        return self.bp.gen2()
+
+class AdditivePoint:
+    """
+    A wrapper for GT points so they use additive notation.
+    """
+    def __init__(self, pt):
+        self.pt = pt
+        self.group = self.pt.group
+
+    def export(self, form=0):
+        return self.pt.export(form) if form else self.pt.export()
+
+    def __mul__(self, nb):
+        return AdditivePoint(self.pt**nb)
+
+    def __eq__(self, other):
+        return self.pt == other.pt
+
+    def __add__(self, other):
+        return AdditivePoint(self.pt*(other.pt))
+
+    __rmul__=__mul__
+
+class PairablePoint:
+    """
+    A wrapper for G1 (resp G2) points so they can be paired with a G2 (resp G1) point by pt.pair(other)
+    """
+    def __init__(self, ecpt):
+        """
+        TODO : decide if self.group returns the GT group or the G1 (or G2) group. If the latter, decide how to determine G1/G2
+        """
+        self.ecpt = ecpt
+        self.group = MyGTGroup(self.ecpt.group)
+    
+    
+    def __add__(other):
+        return PairablePoint(self.ecpt+other.ecpt)
+
+    def __mul__(self, nb):
+        return PairablePoint(self.ecpt*nb)
+    
+    def export(self, form=0):
+        return self.ecpt.export(form) if form else self.ecpt.export()
+
+    def __eq__(self, other):
+        return self.ecpt == other.ecpt
+    
+    __rmul__ = __mul__
+
+    def pair(self, other):
+        return AdditivePoint(self.ecpt.group.pair(self, other))
+
+
+
+class MyG1Group:
+    """
+    A wrapper for the G1 (behaving like an EcGroup) group.
+    """
+    def __init__(self, bp):
+        self.bp = bp
+
+    def generator(self):
+        return self.bp.gen1()
+
+    def infinite(self):
+        return self.generator().inf(self.bp)
+
+    def order(self):
+        return self.bp.order()
+
+class MyG2Group:
+    """
+    A wrapper for the G2 group.
+    """
+    def __init__(self, bp):
+        self.bp = bp
+
+    def generator(self):
+        return self.bp.gen2()
+
+    def infinite(self):
+        return self.generator().inf(self.bp)
+
+    def order(self):
+        return self.bp.order()
         
 
-
-
-
-
-
-
-"""
-model from https://www.oreilly.com/library/view/python-cookbook/0596001673/ch15s10.html:
-
-from _ _future_ _ import nested_scopes
-import new
-
-def enhance_method(klass, method_name, replacement):
-    'replace a method with an enhanced version'
-    method = getattr(klass, method_name)
-    def enhanced(*args, **kwds): return replacement(method, *args, **kwds)
-    setattr(klass, method_name, new.instancemethod(enhanced, None, klass))
-
-def method_logger(old_method, self, *args, **kwds):
-    'example of enhancement: log all calls to a method'
-    print '*** calling: %s%s, kwds=%s' % (old_method._ _name_ _, args, kwds)
-    return_value = old_method(self, *args, **kwds) # call the original method
-    print '*** %s returns: %r' % (old_method._ _name_ _, return_value)
-    return return_value
-
-def demo(  ):
-    class Deli:
-        def order_cheese(self, cheese_type):
-            print 'Sorry, we are completely out of %s' % cheese_type
-
-    d = Deli(  )
-    d.order_cheese('Gouda')
-
-    enhance_method(Deli, 'order_cheese', method_logger)
-    d.order_cheese('Cheddar')
-"""
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
