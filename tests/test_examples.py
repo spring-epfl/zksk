cc0aca84975ee5e53e6eb94ae7f8d9a4aa5622b9
Bogdan Kulynych <hello@bogdankulynych.me>
2019-07-11 13:52:56 +0200

    Add hacky examples testing

---
+++ tests/test_examples.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import importlib
+
+import pytest
+
+PACKAGE_NAME = "zkbuilder"
+EXAMPLES_MOD = "examples"
+BASE_PATH = dir_path = os.path.dirname(os.path.realpath(__file__))
+EXAMPLES_DIR = os.path.join(BASE_PATH, "..", EXAMPLES_MOD)
+EXAMPLE_MODULE_NAMES = [f[:-3] for f in os.listdir(EXAMPLES_DIR) if f.endswith(".py")]
+
+
+@pytest.mark.parametrize("mod", EXAMPLE_MODULE_NAMES)
+def test_examples(mod):
+    importlib.import_module("%s.%s" % (EXAMPLES_MOD, mod), PACKAGE_NAME)
