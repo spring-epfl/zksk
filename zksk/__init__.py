3244ce9bad9886a7d3d204df3621fdbae0b7c48c
Bogdan Kulynych <hello@bogdankulynych.me>
2019-07-14 03:11:56 +0200

    Update description in __init__.py

--- zksk/__init__.py
+++ zksk/__init__.py
@@ -4,9 +4,10 @@ __author__ = "Anonymous"
 __email__ = "john@doe.com"
 __url__ = "https://anonymous.com"
 __license__ = "MIT"
-__description__ = "A simple compiler for composable zero-knowledge proofs"
+__description__ = "Zero-Knowledge Swiss Knife: Python library for prototyping composable zero-knowledge proofs."
 __copyright__ = "Anonymous 2019"
 
 
 from zksk.expr import Secret
 from zksk.primitives.dlrep import DLRep
+from zksk.utils import make_generators
