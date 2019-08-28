c91204d27789b42f162ad137d6b80f284c091d3e
Bogdan Kulynych <hello@bogdankulynych.me>
2019-08-28 15:06:20 +0200

    Add LICENSE

--- zksk/__init__.py
+++ zksk/__init__.py
@@ -1,11 +1,11 @@
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 __title__ = "zksk"
 __author__ = "Wouter Lueks, Bogdan Kulynych, Jules Fasquelle, Simon Le Bail-Collet"
 __email__ = "wouter.lueks@epfl.ch"
 __url__ = "https://zksk.rtfd.io"
 __license__ = "MIT"
 __description__ = "Zero-Knowledge Swiss Knife: Python library for prototyping composable zero-knowledge proofs."
-__copyright__ = "EPFL SPRING Lab 2019"
+__copyright__ = "2019, Wouter Lueks (EPFL SPRING Lab)"
 
 
 from zksk.expr import Secret
