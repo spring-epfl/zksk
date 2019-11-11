3a7e3369a893ea7e22c29a3ee97bd17b91179d5b
Laurent Girod <laurent.girod@epfl.ch>
2019-11-11 11:58:30 +0100

    Clean up.

--- zksk/bn.py
+++ zksk/bn.py
@@ -1,23 +1,8 @@
-if 'BACKEND' not in globals():
-    BACKEND = "relic"
+BACKEND = "openssl"
 
-class Bn:
-    def __new__(cls, *args, **kwargs):
-        if BACKEND == "openssl":
-            from petlib.bn import Bn
-            return Bn.__new__(cls, *args, **kwargs)
-        elif BACKEND == "relic":
-            from petrelic.bn import Bn
-            return Bn.__new__(cls, *args, **kwargs)
-        else:
-            raise NotImplementedError
-
-    def from_hex(*args, **kwargs):
-        if BACKEND == "openssl":
-            from petlib.bn import Bn
-            return Bn.from_hex(*args, **kwargs)
-        elif BACKEND == "relic":
-            from petrelic.bn import Bn
-            return Bn.from_hex(*args, **kwargs)
-        else:
-            raise NotImplementedError
+if BACKEND == "openssl":
+    from petlib.bn import Bn
+elif BACKEND == "relic":
+    from petrelic.bn import Bn
+else:
+    raise NotImplementedError
