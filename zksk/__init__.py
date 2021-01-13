f7f43652a9ef44867b70af9d7ebd2341c64d859a
Laurent Girod <laurent.girod@epfl.ch>
2021-01-13 09:20:14 +0100

    Bump version

--- zksk/__init__.py
+++ zksk/__init__.py
@@ -1,11 +1,11 @@
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __title__ = "zksk"
 __author__ = "Wouter Lueks, Bogdan Kulynych, Jules Fasquelle, Simon Le Bail-Collet"
 __email__ = "wouter.lueks@epfl.ch"
 __url__ = "https://zksk.readthedocs.io"
 __license__ = "MIT"
 __description__ = "Zero-Knowledge Swiss Knife: Python library for prototyping composable zero-knowledge proofs."
-__copyright__ = "2020, Wouter Lueks, Bogdan Kulynych (EPFL SPRING Lab)"
+__copyright__ = "2021, Wouter Lueks, Bogdan Kulynych (EPFL SPRING Lab)"
 
 
 from zksk.expr import Secret
