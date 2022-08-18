cb4fde337ee17bad28e8053f305f3e839e38da13
Paul Staadecker <pstaadec@uwaterloo.ca>
2022-08-18 11:47:12 -0400

    added AND primitive for rsa groups

--- tests/test_rsagroup.py
+++ tests/test_rsagroup.py
@@ -21,3 +21,19 @@ def test_rsagroup_interactive_1():
     verifier = p.get_verifier()
     protocol = SigmaProtocol(verifier, prover)
     assert protocol.verify()
+
+
+def test_rsagroup_and_interactive_1():
+    [g, h] = rsa_dlrep_trusted_setup(bits=1024, num=2)
+    n = g.group.modulus
+    sk1, sk2 = n.random(), n.random()
+    pk1 = sk1 * g
+    pk2 = sk2 * h
+
+    x1 = Secret()
+    x2 = Secret()
+    p = DLRep(pk1, x1 * g) & DLRep(pk2, x2 * h)
+    prover = p.get_prover({x1: sk1, x2: sk2})
+    verifier = p.get_verifier()
+    protocol = SigmaProtocol(verifier, prover)
+    assert protocol.verify()
